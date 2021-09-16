60413f9aabfbfc3391944bdd839d410fe483a63b
Net <netfashion02@gmail.com>
2021-09-16 09:59:42 +0800

    輸出數字更正為整數

--- all.js
+++ all.js
@@ -1,8 +1,41 @@
 const GetId = (id)=> document.getElementById(id);
 let btn = GetId('btn-return');
 
+let isNumber =(val) => {
+    return !isNaN(Number(val));
+}
+
+// let domlist = {    
+//     a:{
+//         name: '坪數',
+//         value: 150
+//     },
+//     b:{
+//         name: '業績',
+//         value: 200
+//     },
+//     c:{
+//         name: '租金',
+//         value: 20
+//     },
+//     d:{
+//         name: '毛利(%)',
+//         value: 52.5
+//     },
+//     e:{
+//         name: '費用(%)',
+//         value: 16
+//     },
+// };
+
+// let domlistLen = Object.keys(domlist).length;
+
 btn.addEventListener('click', (event)=>{
     event.preventDefault();
+    // for(let i = 0; i < domlistLen; i++){        
+    //     domlist[Object.keys(domlist)[i]].value = GetId(Object.keys(domlist)[i]).value;
+    // }
+    // console.log(domlist);
     let a = GetId('a').value;
     let b = GetId('b').value;
     let c = GetId('c').value;
@@ -23,8 +56,8 @@ btn.addEventListener('click', (event)=>{
     let request_num2 = ((b * (d / 100)) - (b * (e / 100)) - c);
     let request_num1_100 = Math.round(request_num1);
     let request_num2_100 = Math.round(request_num2);
-    let request_num3 = request_num1 * 60;
-    let request_num4 = request_num2 * 60;
+    let request_num3 = Math.round(request_num1 * 60);
+    let request_num4 = Math.round(request_num2 * 60);
     let request_num5 = request_num2 * 60 + request_num3;
 
     request1.textContent = request_num1_100;
